9c0241c4b75d423721fcd5b9daca887f8e4ef8fe
Ethan Jones <ejjonesethan@gmail.com>
2019-04-25 15:57:28 -0700

    Figured how to set canvas and produce multiple lines, must find PDF python library

--- testing.py
+++ testing.py
@@ -1,14 +1,22 @@
 #Imports to be used#
 from tkinter import *
 from employee import *
-
+#from PyPDF2 import PdfFileWriter, PdfFileReader
 
 #Main body, creates main window and buttons	
 root = Tk()
-
-C = Canvas(root)
-C.create_line(0,20,300,20)
+canvaswidth = 500
+canvasheight= 500
+y = 20
+x2 = 500
+C = Canvas(root, width = canvaswidth, height = canvasheight)
 C.grid(row = 0)
+
+x = 0
+while x != 10:
+	C.create_line(0, y, x2, y, width = 2.5)
+	y = y + 20
+	x = x + 1
 
 mainMenu = Menu (root)
 
